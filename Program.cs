c08c406fe65fe655ab15d9eae9eb9e99c50be35c
riannf28 <riannurfauzi@student.telkomuniversity.ac.id>
2022-04-09 15:56:35 +0700

    commit pertama

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace modul7_1302200040
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
