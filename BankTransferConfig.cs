ad17925143292cbe3dc6b9c91c7c52f830fd7bdd
riannf28 <riannurfauzi@student.telkomuniversity.ac.id>
2022-04-09 16:50:44 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace modul7_1302200040
+{
+    class BankTransferConfig
+    {
+        public String lang;
+        public String threshold;
+        public String low_fee;
+        public String high_fee;
+        public String methods;
+        public String en;
+        public String id;
+        public void ReadJSON()
+        {
+            String file = "bank_transfer_config.json";
+
+            String str = File.ReadAllText(file);
+
+            BankTransferConfig bank = JsonSerializer.Deserialize<BankTransferConfig>(str);
+
+            Console.WriteLine($"Language{bank.lang} threshold{bank.threshold} low fee{bank.low_fee} high fee{bank.high_fee} methods{bank.methods} en{bank.en} id{bank.id}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,47 @@ namespace modul7_1302200040
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankTransferConfig bank = new BankTransferConfig();
+            bank.ReadJSON();
+            if (bank.lang == "en")
+            {
+                Console.WriteLine("Please insert the amount of money to transfer");
+            } else if (bank.lang == "id")
+            {
+                Console.WriteLine("Masukkan jumlah uang yang akan di-transfer");
+            }
+            if (bank.lang == "EN")
+            {
+                Console.WriteLine("Select transfer method");
+            }
+            else if (bank.lang == "ID")
+            {
+                Console.WriteLine("Pilih metode transfer");
+            }
+            if (bank.lang == "EN")
+            {
+                Console.WriteLine("Please type <CONFIG6> to confirm the transaction");
+            }
+            else if (bank.lang == "ID")
+            {
+                Console.WriteLine("Ketik <CONFIG7> untuk mengkonfirmasi transaksi");
+            }
+            if (bank.lang == "EN")
+            {
+                Console.WriteLine("The transfer is completed");
+            }
+            else if (bank.lang == "ID")
+            {
+                Console.WriteLine("Proses transfer berhasil");
+            }
+            if (bank.lang == "EN")
+            {
+                Console.WriteLine("Transfer is cancelled");
+            }
+            else if (bank.lang == "ID")
+            {
+                Console.WriteLine("Transfer dibatalkan");
+            }
         }
     }
 }
